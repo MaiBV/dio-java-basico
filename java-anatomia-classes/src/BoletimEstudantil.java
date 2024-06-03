b2026f4e513ac96137ca972a55b51d863dbbd5ed
MaiBV <maia.veras@gmail.com>
2024-06-03 15:38:22 -0300

    nov0306

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,21 @@
+public class BoletimEstudantil {
+
+public static void main(String[] args) {
+    int mediaFinal = 7;
+    if(mediaFinal < 6)
+        System.out.println("REPROVADO");
+    else if(mediaFinal == 6)
+        System.out.println("PROVA MINERVA");
+    else
+        System.out.println("APROVADO");  
+}
+
+
+
+
+
+
+
+
+    
+}
--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
     System.out.println(nomeCompleto);
 }
 public static String nomeCompleto (String primeiroNome, String segundoNome) {
-    return primeiroNome.concat(" ").concat(segundoNome);    
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);    
 }
 
 }
