6ce5eb66c882eb03149d04ab96a079173f485313
MaiBV <maia.veras@gmail.com>
2024-06-08 16:17:41 -0300

    080624 16:17

---
+++ Sistema Smart TV/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+
+        System.out.println("Tv Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> Tv ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> Tv ligada ? " + smartTv.ligada);
+
+    }
+}
