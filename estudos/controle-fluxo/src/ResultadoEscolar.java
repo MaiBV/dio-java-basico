a9d79f87237a40f5659c0dd1427a76d1dd816495
MaiBV <maia.veras@gmail.com>
2024-06-10 15:41:08 -0300

    100624 15:40

--- estudos/controle-fluxo/src/ResultadoEscolar.java
+++ estudos/controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,14 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 10;
 
         if (nota >= 7)
             System.out.println("Aprovado");
 
+        else if(nota >= 5 && nota < 7)//true or false
+        System.out.println("Recuperação");
+
         else
-            System.out.println("Reprovado");
+        System.out.println("Reprovado");
     }
 }
---
+++ estudos/controle-fluxo/src/ResultadoEscolarTernaria.java
@@ -0,0 +1,7 @@
+public class ResultadoEscolarTernaria {
+    public static void main(String[] args) {
+        int nota = 6;
+        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota<7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
