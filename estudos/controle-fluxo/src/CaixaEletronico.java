19436ed43dd3a0583dcc45b04c6ab2bf8313b903
MaiBV <maia.veras@gmail.com>
2024-06-10 14:09:02 -0300

    100624 14:10

--- estudos/controle-fluxo/src/CaixaEletronico.java
+++ estudos/controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,14 @@
 public class CaixaEletronico{
     public static void main(String[] args){
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 17;
 
-        if (valorSolicitado < saldo) 
+        if (valorSolicitado < saldo){ 
         saldo = saldo - valorSolicitado;
+        System.out.println("Novo saldo");
 
-        System.out.println(saldo);       
+        }else
+        System.out.println("Saldo insuficiente");
+        System.out.println(saldo);    
     }    
 }
