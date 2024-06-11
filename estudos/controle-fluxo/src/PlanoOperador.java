47cf8576099a0c1a0c7ff2c1362dfa2787a5eab5
MaiBV <maia.veras@gmail.com>
2024-06-10 21:19:07 -0300

    100624 21:19

---
+++ estudos/controle-fluxo/src/PlanoOperador.java
@@ -0,0 +1,16 @@
+public class PlanoOperador {
+    public static void main(String[] args) {
+        String plano = "T"; //M / T
+
+        if(plano =="B") {
+            System.out.println("100 minutos de ligação");
+        }else if(plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e intagram grátis");
+        }else if(plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("Whats e Intagram grátis");
+            System.out.println("5 Gb Youtube");
+        }
+    }
+}
---
+++ estudos/controle-fluxo/src/SistemaMedidaSwitchCase.java
@@ -0,0 +1,21 @@
+public class SistemaMedidaSwitchCase {
+    public static void main(String[] args) {
+        String sigla = "G";
+        switch (sigla) {
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+                case "M":{
+                System.out.println("MEDIO");
+                break;
+            }
+                        case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+                default:
+                System.out.println("INDEFINIDO");
+            }
+    }
+}
