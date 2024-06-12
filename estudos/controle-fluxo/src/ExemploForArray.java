ac0c8f49b234781a30c52d6aed8e9f1e0b8a65aa
MaiBV <maia.veras@gmail.com>
2024-06-12 16:09:04 -0300

    120624 16:08

---
+++ estudos/controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,13 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        
+        //em arrays o indice inicia em ZERO
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x < alunos.length; x++) {
+            
+            System.out.println("O aluno no indice x=" + x + " é " + alunos [ x ] );
+        }
+    }   
+}
