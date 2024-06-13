692df2c4ccf478a9710e7f4b80684382f2edd718
MaiBV <maia.veras@gmail.com>
2024-06-13 13:38:09 -0300

    130624 13:37

---
+++ estudos/controle-fluxo/src/AboutMe.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        try{
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close(); 
+        }
+        catch(InputMismatchException e){
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        }  
+    }
+}
